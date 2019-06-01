779fe8693a597286dd64cda28df807e817710592
XuecaiHu <1312261976@qq.com>
2019-06-01 16:30:06 +0800

    Update option.py

--- option.py
+++ option.py
@@ -19,7 +19,7 @@ parser.add_argument('--seed', type=int, default=1,
                     help='random seed')
 
 # Data specifications
-parser.add_argument('--dir_data', type=str, default='your path to/Meta-SR-Pytorch/',
+parser.add_argument('--dir_data', type=str, default='./',
                     help='dataset directory')
 parser.add_argument('--dir_demo', type=str, default='../test',
                     help='demo image directory')
