67bf1d1ede03876b6edee10d758d970ce9093f44
XuecaiHu <1312261976@qq.com>
2019-06-26 17:15:07 +0800

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -54,7 +54,7 @@ class Model(nn.Module):
             return self.model(x,pos_mat)
 
     def get_model(self):
-        if self.n_GPUs <= 1 or self.args.cpu:
+        if self.n_GPUs <= 1 or self.cpu:
             return self.model
         else:
             return self.model.module
